52b1a5ebcac151ebc8729582c14e9af89bcc5758
laurierpb <laurier.p.b@gmail.com>
2017-01-05 11:04:21 -0500

    test heroku connection database

--- src/main/java/KiboTaiko/Controllers/HomeItemsController.java
+++ src/main/java/KiboTaiko/Controllers/HomeItemsController.java
@@ -14,64 +14,72 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/HomeItems")
 public class HomeItemsController {
-    
+
     /**
      * retourne une liste de tous les home items
+     *
      * @return une liste de tous les home items
      */
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody
     List<HomeItem> getHomeItems() {
+
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        System.out.println(dbUrl);
+
         System.out.println("HomeItemsController : GET");
 
         List<HomeItem> homeItemsList = HomeRepo.getHomeItems();
         return homeItemsList;
     }
-    
+
     /**
      * Insert un nouveau home item
+     *
      * @param homeItem
-     * @return 
+     * @return
      */
     @RequestMapping(method = RequestMethod.POST)
     public HomeItem postCalandrier(@RequestBody HomeItem homeItem) {
-        
-        if(HomeController_Helper.validateHomeItem(homeItem)){
+
+        if (HomeController_Helper.validateHomeItem(homeItem)) {
             System.out.println("Erreur dans l'entree du HomeItem");
-        }else{
+        } else {
             HomeRepo.insertHomeItems(homeItem);
         }
         return homeItem;
     }
-    
+
     /**
      * update un home item passé en paramètre
+     *
      * @param homeItem
      * @param homeItemID
      * @return
      */
     @RequestMapping(method = RequestMethod.PUT, value = "/{homeItemID}")
     public HomeItem putCalandrier(
-            @RequestBody HomeItem homeItem, 
+            @RequestBody HomeItem homeItem,
             @PathVariable int homeItemID) {
-        
+
         System.out.println("HomeItem PUT : " + homeItemID);
-        if(HomeController_Helper.validateHomeItem(homeItem)){
+        if (HomeController_Helper.validateHomeItem(homeItem)) {
             System.out.println("Erreur dans l'entree du HomeItem");
-        }else{
+        } else {
             HomeRepo.updateHomeItems(homeItem);
         }
         return homeItem;
     }
-    
+
     /**
      * Supprime un home item de la base de données
+     *
      * @param homeItemID
      */
     @RequestMapping(method = RequestMethod.DELETE, value = "/{homeItemID}")
     public void deleteHomeItem(
             @PathVariable int homeItemID) {
-        
+
         System.out.println("HomeItem DELETE : " + homeItemID);
         HomeRepo.deleteHomeItems(homeItemID);
     }
