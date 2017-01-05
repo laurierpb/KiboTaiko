9a72e9318bd70216a4a73146c37b749c62622b84
laurierpb <laurier.p.b@gmail.com>
2017-01-05 09:41:20 -0500

    On esseille dequoi pour heroku

--- ddl.sql
+++ ddl.sql
@@ -1,54 +1,53 @@
 ---------------------------------------
 ----        Create des tables      ----
 ---------------------------------------
+-- Table: public.calandrier
+DROP TABLE public.Calandrier;
+CREATE TABLE public.Calandrier
+(
+    Id SERIAL NOT NULL,
+    Image text NOT NULL,
+    ImageAlt text NOT NULL,
+    Contenue text NOT NULL,
+    TitleText text NOT NULL,
+    CONSTRAINT Calandrier_pkey PRIMARY KEY (Id)
+)
+WITH (
+    OIDS=FALSE
+);
 
-     -- Table: public.calandrier
-    DROP TABLE public.Calandrier;
-    CREATE TABLE public.Calandrier
-    (
-      Id SERIAL NOT NULL,
-      Image text NOT NULL,
-      ImageAlt text NOT NULL,
-      Contenue text NOT NULL,
-      TitleText text NOT NULL,
-      CONSTRAINT Calandrier_pkey PRIMARY KEY (Id)
-    )
-    WITH (
-      OIDS=FALSE
-    );
-
-    -- Table: public.homeitems
-    DROP TABLE public.homeitems;
-    CREATE TABLE public.homeitems
-    (
-      ID SERIAL NOT NULL,
-      Ordre integer,
-      Image text NOT NULL,
-      ImageAlt text NOT NULL,
-      Contenue text NOT NULL,
-      Titre text NOT NULL,
-      CONSTRAINT homeitems_pkey PRIMARY KEY (Id)
-    )
-    WITH (
-      OIDS=FALSE
-    );
-    ALTER TABLE public.homeitems
-      OWNER TO postgres;
+-- Table: public.homeitems
+DROP TABLE public.homeitems;
+CREATE TABLE public.homeitems
+(
+    ID SERIAL NOT NULL,
+    Ordre integer,
+    Image text NOT NULL,
+    ImageAlt text NOT NULL,
+    Contenue text NOT NULL,
+    Titre text NOT NULL,
+    CONSTRAINT homeitems_pkey PRIMARY KEY (Id)
+)
+WITH (
+    OIDS=FALSE
+);
+ALTER TABLE public.homeitems
+    OWNER TO postgres;
 
 
 -----------------------------------------
 ----        Autres informations      ----
 -----------------------------------------
 
-    -- Table: public.admin
-    DROP TABLE public.Administrateur;
-    CREATE TABLE public.Administrateur
-    (
-        id SERIAL NOT NULL,
-        username text NOT NULL,
-        password text NOT NULL
-        CONSTRAINT Administrateur_pkey PRIMARY KEY (id)
-    )
-    WITH (
-      OIDS=FALSE
-    );
+-- Table: public.admin
+DROP TABLE public.Administrateur;
+CREATE TABLE public.Administrateur
+(
+    id SERIAL NOT NULL,
+    username text NOT NULL,
+    password text NOT NULL
+    CONSTRAINT Administrateur_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS=FALSE
+);
--- src/main/resources/static/JS/CalandrierController.js
+++ src/main/resources/static/JS/CalandrierController.js
@@ -5,7 +5,7 @@ app.controller('CalandrierController', function ($scope, $http) {
         url: '/Calandrier'
     }).then(function successCallback(response) {        
         $scope.schedule = response.data;
-    }, function errorCallback() {
+    }, function errorCallback(data, status, headers, config) {
         console.log('Le GET vers la ressourse s\'est mal fait');
     });
 });
