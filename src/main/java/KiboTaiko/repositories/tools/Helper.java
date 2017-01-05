41301ea7e33048525e9ff28a8c9077cfa2e2e2da
laurierpb <laurier.p.b@gmail.com>
2017-01-05 12:22:06 -0500

    test heroku connection database

--- src/main/java/KiboTaiko/repositories/tools/Helper.java
+++ src/main/java/KiboTaiko/repositories/tools/Helper.java
@@ -1,5 +1,6 @@
 package KiboTaiko.repositories.tools;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,8 +8,14 @@ import java.sql.SQLException;
 
 public class Helper {
     public static Connection getConnection() throws URISyntaxException, SQLException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        System.out.println("\n\nconnection : "+dbUrl + "\n\n");
-    return DriverManager.getConnection(dbUrl);
-}
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+                + dbUri.getPort() + dbUri.getPath()
+                + "?sslmode=require";
+        System.out.println("\n\n\n" + dbUrl + "\n\n\n");
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
 }
