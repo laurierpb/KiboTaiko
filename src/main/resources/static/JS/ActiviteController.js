e3863431cad81ccbecf0087eb9b01c5327420632
laurierpb <laurier.p.b@gmail.com>
2017-01-11 08:56:11 -0500

    Commit pour heroku

--- src/main/resources/static/JS/ActiviteController.js
+++ src/main/resources/static/JS/ActiviteController.js
@@ -1,4 +1,25 @@
 /* global app */
 app.controller('ActiviteController', function ($scope, $http) {
-    $scope.name = 'ActiviteController';
+    $scope.activites = [{
+            toggle: true,
+            name: "activite1"
+        },{
+            toggle: true,
+            name: "activite2"
+        },{
+            toggle: true,
+            name: "activite3"
+        },{
+            toggle: true,
+            name: "activite4"
+        }
+    ];
+    $scope.toggle = function (element) {
+        element.toggle = element.toggle === false ? true: false;
+        for(var i = 0 ; i < $scope.activites.length ; i++){
+            if($scope.activites[i] !== element){
+               $scope.activites[i].toggle = true; 
+            }
+        }
+    };
 });
